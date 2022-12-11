c7172a3bfd04294e4dbc41ecea4e149163572cf7
Robert Goss <goss.robert@gmail.com>
2022-12-11 07:49:30 +0000

    Add ambiant illumination

--- src/ray.rs
+++ src/ray.rs
@@ -2,6 +2,8 @@
 //
 // Current quadratic and only supports spheres and planes
 
+use std::f64::consts::PI;
+
 use cgmath::{prelude::*};
 use cgmath::{point3, vec3, Point3, Vector3, Matrix4, Deg};
 
@@ -42,6 +44,8 @@ struct Scene<'a> {
     materials : &'a Materials,
     spheres : Vec<Entity<'a>>,
     background : Rgb<u8>,
+    background_illumination : u8,
+    use_ambiant_illumination : bool,
     resolution : (usize, usize),
     max_depth : u8
 }
@@ -97,6 +101,67 @@ impl<'a> Contact<'a> {
         let dir = ray_in.direction - align * self.normal;
         Ray{start: self.pos, direction : dir}
     }
+
+    fn light_rays5(self : &Self) -> [UnitVector3;5] {
+        let inv_sqrt2 = 0.5_f64.sqrt();
+        let u: UnitVector3 = if self.normal.x.abs() > 1.0-1e-6 {
+            (vec3(1.0,0.0,0.0) - self.normal.x * self.normal).normalize()
+        } else {
+            (vec3(0.0,1.0,0.0) - self.normal.x * self.normal).normalize()
+        };
+        let v = self.normal.cross(u);
+        [
+            self.normal,
+            inv_sqrt2 * (u + self.normal),
+            inv_sqrt2 * (-u + self.normal),
+            inv_sqrt2 * (v + self.normal),
+            inv_sqrt2 * (-v + self.normal)
+        ]
+    }
+
+    fn light_rays9(self : &Self) -> [UnitVector3;9] {
+        let inv_sqrt2 = 0.5_f64.sqrt();
+        let u: UnitVector3 = if self.normal.x.abs() > 1.0-1e-6 {
+            (vec3(1.0,0.0,0.0) - self.normal.x * self.normal).normalize()
+        } else {
+            (vec3(0.0,1.0,0.0) - self.normal.x * self.normal).normalize()
+        };
+        let v = self.normal.cross(u);
+        let lambda = 3.0 * inv_sqrt2 / 5.0;
+        let n1 = inv_sqrt2 * self.normal;
+        let n2 = 4.0/5.0 * self.normal;
+        [
+            self.normal,
+            (inv_sqrt2 * u) + n1,
+            (inv_sqrt2 * -u) + n1,
+            (inv_sqrt2 * v) + n1,
+            (inv_sqrt2 * -v) + n1,
+            (lambda * (u+v)) + n2,
+            (lambda * (u-v)) + n2,
+            (lambda * (-u+v)) + n2,
+            (lambda * (-u-v)) + n2,
+        ]
+    }
+
+    fn light_rays_fine(&self) -> Vec<UnitVector3> {
+        let u: UnitVector3 = if self.normal.x.abs() > 1.0-1e-6 {
+            (vec3(1.0,0.0,0.0) - self.normal.x * self.normal).normalize()
+        } else {
+            (vec3(0.0,1.0,0.0) - self.normal.x * self.normal).normalize()
+        };
+        let v = self.normal.cross(u);
+        let num = 128;
+        let turns = 5;
+        // Spiral up - warning too much trig
+        (0..num).map(
+            |i| {
+                let theta = (i as f64 * PI) / (2.0 * num as f64);
+                let phi = (i as f64 * PI * 2.0 * turns as f64) / (num as f64);
+                theta.sin() * self.normal 
+                + theta.cos() * (phi.sin() * u + phi.cos() * v)
+            }
+        ).collect()
+    }
 }
 
 impl<'a> Scene<'a> {
@@ -104,6 +169,12 @@ impl<'a> Scene<'a> {
         self.find_best_contact(ray).map(
             |contact| self.trace_contact(ray, &contact, depth)
         ).unwrap_or(self.background)
+    }
+
+    fn trace_illumination(self : &Self, ray : &Ray, depth : u8) -> u8 {
+        self.find_best_contact(ray).map(
+            |contact| self.trace_contact_illumination(&contact, false, depth)
+        ).unwrap_or(self.background_illumination)
     }
 
     fn find_best_contact(self : &Self, ray : &Ray) -> Option<Contact> {
@@ -120,7 +191,67 @@ impl<'a> Scene<'a> {
         } else {
             Some(self.trace_ray(&contact.reflection_ray(ray), depth - 1))
         };
-        contact.material.colour(self.materials, &contact.local_pos, relfection)
+        let illumination = if self.use_ambiant_illumination {
+            if depth == 0 {
+                self.trace_contact_illumination(contact, depth == self.max_depth, 1)
+            } else {
+                self.trace_contact_illumination(contact, depth == self.max_depth, 2)
+            }
+        } else {
+            255
+        };
+        contact.material.colour(
+            self.materials,
+             &contact.local_pos, 
+             illumination,
+             relfection
+        )
+    }
+
+    fn trace_contact_illumination(self : &Self, contact : &Contact, fine : bool, depth : u8) -> u8 {
+        let surface_glow = contact.material.illumination(
+            self.materials,
+             &contact.local_pos
+        ) as u32;
+        let merged_light :u32 = if fine {
+            let fine_dirs = contact.light_rays_fine();
+            let num = fine_dirs.len() as u32;
+            fine_dirs.into_iter().map(
+                |dir| self.trace_illumination(
+                    &Ray { start: contact.pos, direction : dir }, 
+                    1
+                ) as u32
+            ).sum::<u32>() / num
+        } else {
+            if depth == 0 {
+                0
+            } else {
+                if depth == 1 {
+                    contact.light_rays5().into_iter().map(
+                        |dir| self.trace_illumination(
+                            &Ray { start: contact.pos, direction : dir }, 
+                            0
+                        ) as u32
+                    ).sum::<u32>() / 5
+                } else {
+                    contact.light_rays9().into_iter().map(
+                        |dir| self.trace_illumination(
+                            &Ray { start: contact.pos, direction : dir }, 
+                            depth - 1
+                        ) as u32
+                    ).sum::<u32>() / 9
+                }
+            }
+        };
+        if surface_glow != 0 {
+            if surface_glow + merged_light > 255 {
+                255
+            } else {
+                (surface_glow + merged_light) as u8
+            }
+        } else { 
+            merged_light as u8
+        }
     }
 
     fn from_json<'geom, 'mat>(
@@ -133,6 +264,7 @@ impl<'a> Scene<'a> {
         let res_x = input["resolution_x"].as_usize().unwrap_or(1024);
         let res_y = input["resolution_y"].as_usize().unwrap_or(1024);
         let max_depth = input["max_depth"].as_u8().unwrap_or(4);
+        let use_ambiant = input["ambiant_lighting"].as_bool().unwrap_or(false);
         let entities : Vec<Entity> = input["entities"].members().filter_map(
             |value| Entity::from_json(value, geometry, materials) 
         ).collect();
@@ -141,9 +273,11 @@ impl<'a> Scene<'a> {
             camera : Camera{fov : 1.0,  pos : point3(0.0,0.0,1.0)},
             resolution : (res_x, res_y),
             background : Rgb([200,200,200]),
+            background_illumination : 240,
             spheres : entities,
             max_depth : max_depth,
-            materials : materials
+            materials : materials,
+            use_ambiant_illumination: use_ambiant
         })
     }
 
--- src/ray/materials.rs
+++ src/ray/materials.rs
@@ -5,6 +5,7 @@ use cgmath::Point3;
 
 pub struct Colour {
     colour : Rgb<u8>,
+    glow : u8,
     reflectivity : u8
 }
 
@@ -27,7 +28,7 @@ impl Materials {
         let mut materials = Materials {
             names : HashMap::new(),
             materials : Vec::new(),
-            default : Colour { colour: Rgb([145,145,145]), reflectivity: 0 }
+            default : Colour { colour: Rgb([145,145,145]), glow: 0, reflectivity: 0 }
         };
         for (mat_name, mat_input) in input["materials"].entries() {
             if let Some(mat) = parse_material(mat_input, &materials) {
@@ -75,13 +76,30 @@ fn merge_colour(c1 : Rgb<u8>, c2 : Rgb<u8>, par : u8 ) -> Rgb<u8> {
     Rgb([r as u8, g as u8, b as u8])
 }
 
+fn scale_colour(c : Rgb<u8>, par : u8 ) -> Rgb<u8> {
+    let r1 = par as u16 * c.0[0] as u16;
+    let g1 = par as u16 * c.0[1] as u16;
+    let b1 = par as u16 * c.0[2] as u16;
+    let r = r1 / 255;
+    let g = g1 / 255;
+    let b = b1 / 255;
+    Rgb([r as u8, g as u8, b as u8])
+}
+
 pub trait Material {
     fn colour(
         &self, 
         mats : &Materials,
         local : &Point3<f64>, 
+        illumination : u8,
         reflection : Option<Rgb<u8>>
     ) -> Rgb<u8>;
+
+    fn illumination(
+        &self,
+        mats : &Materials,
+        local : &Point3<f64>
+    ) -> u8;
 }
 
 impl Colour {
@@ -89,11 +107,12 @@ impl Colour {
         let r = input["r"].as_u8()?;
         let g = input["g"].as_u8()?;
         let b = input["b"].as_u8()?;
+        let glow = input["glow"].as_u8().unwrap_or(0);
         let refl = input["reflect"].as_f64()?;
         let refl_u8 = (refl * 255.0) as u8;
         Some(
             Box::new(
-                Colour { colour: Rgb([r,g,b]), reflectivity: refl_u8 }
+                Colour { colour: Rgb([r,g,b]), glow : glow, reflectivity: refl_u8 }
             )
         )
     }
@@ -123,14 +142,34 @@ impl Material for Colour {
         &self,
         _mats : &Materials,
         _local_point : &Point3<f64>,
+        illumination : u8,
         reflection : Option<Rgb<u8>>
     ) -> Rgb<u8> {
-        merge_colour(
+        let refl_colour = merge_colour(
             reflection.unwrap_or(Rgb([0,0,0])), 
             self.colour,
             self.reflectivity
-        )
+        );
+        if self.glow != 0 {
+            let g_illumination = illumination as u16 + self.glow as u16;
+            if g_illumination > 255 {
+                refl_colour
+            } else {
+                scale_colour(refl_colour, g_illumination as u8)
+            }
+        } else {
+            scale_colour(refl_colour, illumination)
+        }
     }
+
+    fn illumination(
+        &self,
+        _mats : &Materials,
+        _local : &Point3<f64>
+    ) -> u8 {
+        self.glow
+    }
+
 }
 
 impl Material for Checker {
@@ -138,12 +177,25 @@ impl Material for Checker {
         &self,
         mats : &Materials,
         local_point : &Point3<f64>,
+        illumination : u8,
         reflection : Option<Rgb<u8>>,
     ) -> Rgb<u8> {
         let x = (local_point.x * self.step) as i64;
         let y = (local_point.y * self.step) as i64;
         let z = (local_point.z * self.step) as i64;
         let index = if (x+y+z) % 2 == 0 { self.material_even } else { self.material_odd };
-        mats.materials[index].colour(mats, local_point, reflection)
+        mats.materials[index].colour(mats, local_point, illumination, reflection)
+    }
+
+    fn illumination(
+        &self,
+        mats : &Materials,
+        local_point : &Point3<f64>
+    ) -> u8 {
+        let x = (local_point.x * self.step) as i64;
+        let y = (local_point.y * self.step) as i64;
+        let z = (local_point.z * self.step) as i64;
+        let index = if (x+y+z) % 2 == 0 { self.material_even } else { self.material_odd };
+        mats.materials[index].illumination(mats, local_point)
     }
 }
